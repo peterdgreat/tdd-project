1c3c23bbcb23540798f02990f0d3431aa27c1340
Ajayi Peter <peterdgreat2016@gmail.com>
2021-12-23 20:19:12 +0100

    TDD test for factorial function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,5 +6,17 @@ describe Solver do
       solver = Solver.new
       expect(solver).to be_a Solver
     end
+    it 'should return a factorial of 5 as 120' do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eq(120)
+    end
+    it 'should return a factorial of 0 as 1' do
+      solver = Solver.new
+      expect(solver.factorial(0)).to eq(1)
+    end
+    it 'shoulld return a factorial of negative number as error' do
+      solver = Solver.new
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
   end
 end
