4a6144044433de348d8ca690844fcd553c3c2ed9
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-12-23 16:21:15 -0300

    Add tests for reverse method and fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -14,9 +14,19 @@ describe Solver do
       solver = Solver.new
       expect(solver.factorial(0)).to eq(1)
     end
-    it 'shoulld return a factorial of negative number as error' do
+    it 'should return a factorial of negative number as error' do
       solver = Solver.new
       expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
+    it 'should call reverse method and return the reverse of a word' do
+      solver = Solver.new
+      result = solver.reverse('hello')
+      expect(result).to eq('olleh')
+    end
+    it 'should call fizzbuzz method passing a 3 and return fizz' do
+      solver = Solver.new
+      result = solver.fizzbuzz(3)
+      expect(result).to eq('fizz')
     end
   end
 end
