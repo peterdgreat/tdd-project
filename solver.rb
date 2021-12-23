b68c9833e7c10f47ca95546175780609fb61ec9e
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-12-23 17:28:06 -0300

    Reformat code to fix rubocop linters

--- solver.rb
+++ solver.rb
@@ -1,29 +1,25 @@
 class Solver
-    def initialize
-        
+  def initialize; end
+
+  def factorial(number)
+    raise ArgumentError, 'n must be a positive number' if number.negative?
+
+    (1..number).inject(:*) || 1
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
     end
-    def factorial(n)
-        raise ArgumentError, "n must be a positive number" if n < 0
-        f = 1
-        for i in 1..n
-            f *= i
-        end 
-        f
-    end
-    def reverse(s)
-        s.reverse
-    end
-    def fizzbuzz(number)
-        if number%3 == 0 && number%5  == 0 
-            'fizzbuzz'
-        elsif number%3 == 0
-            'fizz'
-        elsif number%5 == 0
-            'buzz'
-        else 
-            number.to_s
-        end
-    end
+  end
 end
-
-
